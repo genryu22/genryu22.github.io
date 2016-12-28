f93d70d4827f2602f68fddde2345b00d7016caca
hakucha <genryu7@gmail.com>
2016-12-28 16:14:01 +0900

    delete unneeded files

--- tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tile" tilewidth="32" tileheight="32" tilecount="160" columns="8">
- <image source="assets/50309019_p0.png" trans="000000" width="256" height="640"/>
-</tileset>
